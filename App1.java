101d5ea0e88b6b5ee0856bcebff05bbad39fe9f9
Laasya2002 <laasya4502@gmail.com>
2024-02-01 13:43:14 +0530

    1 st commit by Laasya

--- App1.java
+++ App1.java
@@ -1,6 +1,6 @@
 public class App1{
 
-    public static void main(String[] args){
+    public static void main(String args[]){
         System.out.println("Java App Pulled & Build by Jenkins");
     }
 }
