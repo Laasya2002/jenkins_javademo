5430d5916143b84e44f796b0602677200d968eec
Laasya2002 <77779819+Laasya2002@users.noreply.github.com>
2024-02-01 20:11:26 +0530

    Update Webhooks.java

--- Webhooks.java
+++ Webhooks.java
@@ -2,5 +2,6 @@ public class Webhooks{
     public static void main(String[] args) {
         System.out.println("Webhooks page");
         System.out.println("Working...");
+        System.out.println("Working..............");
     }
 }
